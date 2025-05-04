fc293c07ba2b334a83e47b9e4c2a99cf766e8b9d
Riju-bak <defaltsith@gmail.com>
2025-05-03 22:40:07 -0400

    Shitty Handle APIVersions requests

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
 
+namespace Kafka;
+
 public class Server
 {
     public static async Task Main(string[] args)
@@ -17,20 +19,100 @@ public class Server
         byte[] buffer = new byte[1024]; //1KB buffer
         int bytesRead = await clientSocket.ReceiveAsync(buffer);
 
-        int messageSize = GetMessageSize(buffer), correlationId = GetCorrelationId(buffer);
+        int correlationId = GetCorrelationId(buffer);
+        
+        byte[] requestApiKeyBytes = GetBigEndianBytes((Int16)ParseApiKey(buffer));
+        byte[] apiMinVersionBytes = GetBigEndianBytes((Int16)0);
+        byte[] apiMaxVersionBytes = GetBigEndianBytes((Int16)4);
+
         int requestApiVersion = ParseApiVersion(buffer);
+        byte[] correlationIdBytes = GetBigEndianBytes(correlationId);
+       
+        Int16 errorCode = 0;
+        if (requestApiVersion < 0 || requestApiVersion > 4)
+            errorCode = 35; //UNSUPPORTED_VERSION
         
-        byte[] messageSizeBytes = GetBigEndianBytes(messageSize), correlationIdBytes = GetBigEndianBytes(correlationId);
-
-        Int16 errorCode = 35;
         byte[] errorCodeBytes = GetBigEndianBytes(errorCode);
-
-        byte[] result = new byte[messageSizeBytes.Length + correlationIdBytes.Length + errorCodeBytes.Length];
-        Buffer.BlockCopy(messageSizeBytes, 0, result, 0, messageSizeBytes.Length);
-        Buffer.BlockCopy(correlationIdBytes, 0, result, messageSizeBytes.Length, correlationIdBytes.Length);
-        Buffer.BlockCopy(errorCodeBytes, 0, result, messageSizeBytes.Length + correlationIdBytes.Length, errorCodeBytes.Length);
         
-        await clientSocket.SendAsync(result);
+        int responseHeaderSize = correlationIdBytes.Length; //32 bits (4 B)
+
+        int numberOfSupportedApiKeys = 1; //Currently only support api_key=18 aka ApiVersions
+        int responseBodySize = 2 //error_code (2B)
+                               + 1 //num_api_keys (1B) num_api_keys = 1 for now. We only support api_key 18
+                               + (
+                                   2 //api_key          (2B)
+                                   + 2 //min_version    (2B)
+                                   + 2 //max_version    (2B)
+                                   + 1 //tagged_field/TAG_BUFFER (1B)
+                               ) * numberOfSupportedApiKeys
+                               + 4 //throttle_time_ms (4B)
+                               + 1; //tagged_field/TAG_BUFFER     (1B)
+
+        int responseMessageSize = responseHeaderSize + responseBodySize;  //4B + Body Size in B 
+        byte[] responseMessageSizeBytes = GetBigEndianBytes(responseMessageSize);
+
+        int responseTotalSize = 4 + responseHeaderSize + responseBodySize;
+        
+    //TODO: Improve this whole block using structs/classes. 
+        byte[] response = new byte[responseTotalSize];
+        
+        int dstOffset = 0;
+        //Message Size
+        Buffer.BlockCopy(responseMessageSizeBytes, 0, response, 0, responseMessageSizeBytes.Length);
+        dstOffset += responseMessageSizeBytes.Length;
+        
+        //Header
+        Buffer.BlockCopy(correlationIdBytes, 0, response, dstOffset, correlationIdBytes.Length);
+        dstOffset += correlationIdBytes.Length;
+        
+        // ----  Body  -----
+        Buffer.BlockCopy(errorCodeBytes, 0, response, dstOffset, errorCodeBytes.Length);
+        dstOffset += errorCodeBytes.Length;
+            
+            //num_api_keys
+            response[dstOffset] = 0x02; //codecrafters tester is buggy, it decodes 2 as 1 
+            dstOffset += 1;
+
+            //api_key
+            byte[] responseApiKeyBytes = requestApiKeyBytes;
+            Buffer.BlockCopy(responseApiKeyBytes, 0, response, dstOffset, responseApiKeyBytes.Length);
+            dstOffset += responseApiKeyBytes.Length;
+            
+            //min_version
+            Buffer.BlockCopy(apiMinVersionBytes, 0, response, dstOffset, apiMinVersionBytes.Length);
+            dstOffset += apiMinVersionBytes.Length;
+            
+            //max_version
+            Buffer.BlockCopy(apiMaxVersionBytes, 0, response, dstOffset, apiMaxVersionBytes.Length);
+            dstOffset += apiMaxVersionBytes.Length;
+            
+            // tag_buffer TODO: Figure out what the hell this is!!
+             response[dstOffset] = 0x00;
+             dstOffset++;
+
+            //throttle_time_ms
+            for (int i = dstOffset; i < dstOffset + 4; i++)
+                response[i] = 0x00;
+            dstOffset += 4;
+
+            //tag_buffer
+            response[dstOffset] = 0x00;
+            dstOffset++;
+        // --Body end ---
+
+    //  TODO: Block to improve end /////////////////
+     
+            await clientSocket.SendAsync(response);
+    }
+
+    private static Int16 ParseApiKey(byte[] reqBuffer)
+    {
+        int apikeyInt32 = 0;
+        for(int i=4; i<6; i++)
+            apikeyInt32 += reqBuffer[i] << (8 * (1 - (i-4)));
+        Int16 apiKey = (Int16)apikeyInt32;
+        Console.WriteLine($"Request api-key: {apiKey}");
+        return apiKey;
     }
 
     static int ParseApiVersion(byte[] reqBuffer)
